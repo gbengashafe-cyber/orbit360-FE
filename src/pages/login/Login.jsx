e2a858976f0d63d786d026ce149535fdfdd54eeb
Oluwaseun Abiola <oluabiolaseun@gmail.com>
2026-03-04 02:15:48 +0100

    feat: new login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -8,6 +8,7 @@ import { localStorageKeys, LocalStorageUtil } from '@/utils/local-storage.util';
 import { Eye, EyeOff } from 'lucide-react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
+import logo from '../../orbit360_logo-new.png';
 
 export const LoginPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -49,7 +50,7 @@ export const LoginPage = () => {
         {/* Left panel */}
         <div className="rounded-[2.5rem] flex-1 flex flex-col text-center lg:text-left lg:justify-center py-6 lg:py-10 px-7 lg:px-14 min-w-0 bg-white/85">
           <div className="w-40 mx-auto lg:mx-0">
-            <img src="../../../orbit360_logo-new.png" />
+            <img src={logo} />
           </div>
           <div className="mx-auto lg:mx-0">
             <h1 className="w-full lg:w-[22ch] text-center lg:text-left text-2xl lg:text-4xl font-bold text-blue-700 leading-tight mb-4 tracking-tight">
@@ -64,7 +65,7 @@ export const LoginPage = () => {
         {/* Right panel */}
         <div className="absolute top-[45%] lg:-top-8 lg:-bottom-8 left-4 lg:left-auto right-4 lg:right-16 grid bg-white py-9 lg:w-[36%] rounded-[2.5rem]">
           <div className="w-40 mx-auto hidden lg:block">
-            <img src="../../../orbit360_logo-new.png" />
+            <img src={logo} />
           </div>
 
           <form className="w-[75%] mx-auto block" onSubmit={handleSubmit}>
